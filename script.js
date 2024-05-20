94477c8d2f196aaee3f7bdd1fe3a5912fc2365b3
satishrathod-dev <167071223+satishrathod-dev@users.noreply.github.com>
2024-05-20 16:08:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 function fibonacci(n) {
 // your code here
-	if(n === 1){
-		return 0;
-	}
-	else if(n===2){
-		return 1;
-	}
-	else{
-		return fibonacci(n-1) + fibonacci(n-2);	
-	}
+	// if(n === 1){
+	// 	return 0;
+	// }
+	// else if(n===2){
+	// 	return 1;
+	// }
+	// else{
+	// 	return fibonacci(n-1) + fibonacci(n-2);	
+	// }
+	if (n <= 1)
+        return n;
+
+    let previousFib = 0, currentFib = 1;
+    for (let i = 2; i <= n; i++) {
+        let newFib = previousFib + currentFib;
+        previousFib = currentFib;
+        currentFib = newFib;
+    }
+
+    return currentFib;
 }
 
 module.exports = fibonacci;
